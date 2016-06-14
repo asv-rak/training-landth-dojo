17dc12d8c50ae5f78c0800845378799b047107eb
asv-landth <lando@MAC008.local>
2016-06-14 14:17:47 +0700

    Change require script method

--- gae_django_guestbook/static/js/helloDojo.js
+++ gae_django_guestbook/static/js/helloDojo.js
@@ -3,8 +3,33 @@
  */
 //alert("Hello Dojo");
 
+define([
+    // The dojo/dom module is required by this module, so it goes
+    // in this list of dependencies.
+    'dojo/dom'
+], function(dom){
+    // Once all modules in the dependency list have loaded, this
+    // function is called to define the demo/myModule module.
+    //
+    // The dojo/dom module is passed as the first argument to this
+    // function; additional modules in the dependency list would be
+    // passed in as subsequent arguments.
 
-require([
+    var greatingString = "n/a";
+
+    // This returned object becomes the defined value of this module
+    return {
+        setText: function(text){
+            greatingString = text;
+        },
+        print: function (id, text) {
+            alert(greatingString);
+        }
+    };
+});
+
+
+/*require([
     'dojo/dom',
     'dojo/query',
     'dojo/fx',
@@ -13,6 +38,12 @@ require([
     "dojo/dom-construct",
     'dojo/domReady!',
 ], function(dom, query, fx, domStyle, win, domConstruct){
+
+    return {
+        print: function(){
+
+        }
+    };
 
     var n = domConstruct.create(
         "div",
@@ -25,4 +56,4 @@ require([
         top: 100,
         left: 200
     }).play();
-});
+});*/
