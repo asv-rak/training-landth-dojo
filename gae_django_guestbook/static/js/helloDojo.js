da4c5c0138c7d37a4f7943637edfbebf80e79748
asv-landth <lando@MAC008.local>
2016-06-14 17:17:06 +0700

    Fix code style

--- gae_django_guestbook/static/js/helloDojo.js
+++ gae_django_guestbook/static/js/helloDojo.js
@@ -3,29 +3,39 @@
  */
 
 define([
-    'dojo/dom',
-    'dojo/dom-construct',
-    'dojo/_base/window'
-], function(dom, domConstruct, win){
+	'dojo/dom',
+	'dojo/dom-construct',
+	'dojo/_base/window'
+], function (dom, domConstruct, win) {
 
-    var greatingString = null;
+	var greatingString = null;
 
-    return {
-        setText: function(text){
-            greatingString = text;
-        },
-        getText: function(){
-            return greatingString;
-        },
-        alert: function () {
-            alert(greatingString);
-        },
-        print: function(){
-            domConstruct.create(
-                'div',
-                { innerHTML: this.getText(), style: {color: 'green', fontSize: '30px', fontWeight: 'bold'} },
-                win.body()
-            );
-        }
-    };
+	return {
+		setText: function (text) {
+			greatingString = text;
+		},
+
+		getText: function () {
+			return greatingString;
+		},
+
+		alert: function () {
+			alert(greatingString);
+		},
+
+		print: function () {
+			domConstruct.create(
+					'div',
+					{
+						innerHTML: this.getText(),
+						style: {
+							color: 'green',
+							fontSize: '30px',
+							fontWeight: 'bold'
+						}
+					},
+					win.body()
+			);
+		}
+	};
 });
