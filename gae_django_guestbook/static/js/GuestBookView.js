4630092b6bdf9037f91d611dfdd0c54927cc4cfd
asv-landth <lando@MAC008.local>
2016-06-15 15:48:23 +0700

    Add code inherited

--- gae_django_guestbook/static/js/GuestBookView.js
+++ gae_django_guestbook/static/js/GuestBookView.js
@@ -3,51 +3,64 @@
  */
 define([
 	"dojo/_base/declare",
-	"dijit/_WidgetBase",
-	"dijit/_WidgetsInTemplateMixin",
-	"dijit/_TemplatedMixin",
+	"greeting/_ViewBase",
 	"dojo/text!./templates/guestBookWidget.html",
-	"dojo/dom-construct"
-], function (declare, _WidgetBase, _WidgetsInTemplateMixin, _TemplatedMixin, template, domConstruct) {
+	"dojo/dom-construct",
+	"greeting/Greeting"
+], function (declare, _ViewBase, template, domConstruct, _Greeting) {
 
-	return declare([_WidgetBase, _TemplatedMixin], {
-
-		//
-		accessClass: 'access',
-		greetingClass: 'greeting',
-		gbClass: 'guestbook',
-		contentLabel: 'dataInput',
-
-		//
-		greetingContent: '',
-		guestBookName: '',
+	return declare([_ViewBase], {
 
 		templateString: template,
 
+		currentGuestBookName: '',
+
+		greetingClass: 'greeting',
+		guestBookClass: 'guestbook',
+		contentLabel: 'dataInput',
+
+		greetingContent: '',
+		guestBookName: '',
+
 		postCreate: function () {
+			this.initGuestBook();
+		},
+
+		initGuestBook: function () {
+			var greeting_01 = new _Greeting('1', 'guestbook 01 - greeting 01');
 			var guestBookList = [
-				{guestBookName: '1', greetingList: [
-					{content: 'guestbook 01 - greeting 01'},
-					{content: 'guestbook 01 - greeting 02'}
-				]},
-				{guestBookName: '2', greetingList: [
+				{
+					guestBookName: '1', greetingList: [
+						{content: 'guestbook 01 - greeting 01'},
+						{content: 'guestbook 01 - greeting 02'}
+					]
+				},
+				{
+					guestBookName: '2', greetingList: [
 					{content: 'guestbook 02 - greeting 01'}
-				]},
+				]
+				},
 				{guestBookName: '3', greetingList: []},
-				{guestBookName: '4', greetingList: [
+				{
+					guestBookName: '4', greetingList: [
 					{content: 'guestbook 04 - greeting 01'}
-				]},
-				{guestBookName: '5', greetingList: [
+				]
+				},
+				{
+					guestBookName: '5', greetingList: [
 					{content: 'guestbook 05 - greeting 01'},
 					{content: 'guestbook 05 - greeting 02'},
 					{content: 'guestbook 05 - greeting 03'}
-				]}
+				]
+				}
 			];
-			var selectNode = this.gbNameListNode;
+
+			var selectNode = this.guestBookNameListNode;
 			for (var i = 0; i < guestBookList.length; i++) {
 				var option = selectNode.appendChild(domConstruct.toDom('<option>' + guestBookList[i].guestBookName + '</option>'));
 			}
-		}
 
+
+		}
 	});
 });
