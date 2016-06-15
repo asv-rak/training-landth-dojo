00e1dd5ef1e9a607ddb615c391a80345958cbd6a
asv-landth <lando@MAC008.local>
2016-06-15 15:05:10 +0700

    Create _ViewBase

---
+++ gae_django_guestbook/static/js/_ViewBase.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lando on 6/15/16.
+ */
+
+define([
+	"dojo/_base/declare",
+	"dijit/_WidgetBase",
+	"dijit/_WidgetsInTemplateMixin",
+	"dijit/_TemplatedMixin"
+], function (declare, _WidgetBase, _WidgetsInTemplateMixin, _TemplatedMixin) {
+
+	return declare([_WidgetBase, _WidgetsInTemplateMixin, _TemplatedMixin], {});
+});
